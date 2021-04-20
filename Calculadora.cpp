2de8ec7fb83e0be57781aaf3257f92fe3a534b6b
KB1te <63610437+KB1te@users.noreply.github.com>
2021-04-20 07:44:06 -0300

    fix some issues >.<

--- Calculadora.cpp
+++ Calculadora.cpp
@@ -1,34 +1,19 @@
 #include <iostream>
-
-using namespace std;
-
 int main () {
-    bool loop = true;
-    while ( loop == true ) {
-
-        int numero1;
-        int numero2;
-
-        cout << "=======================" << endl << endl;
-        cout << "  M0rp's Calculator" << endl << endl;
-        cout << "=======================" << endl << endl;
-
-        cout << "Type the first number : ";
-        cin >> numero1;
-        if(numero1 <= 0){
-            cout << "Desculpe mas voce escreveu um numero negativo ou o numero 0, ou uma letra..." << endl;
-            loop = false;
-        }else{
-            cout << "Type the second number : ";
-            cin >> numero2;
-            cout << "" << endl;
-            cout << "A soma:" << " " << numero1 + numero2 << endl << endl;
-            cout << "A subtracao:" << " " << numero1 - numero2 << endl << endl;
-            cout << "A divisao:" << " " << numero1 / numero2 << endl << endl;
-            cout << "A multiplicacao:" << " " << numero1 * numero2 << endl << endl;
-
-}
-
-}
-
+    do{
+        float num1=0,num2=0;
+        std::cout << "=======================" << std::endl;
+        std::cout << "  M0rp's Calculator" << std::endl;
+        std::cout << "=======================" << std::endl;
+        std::cout << "Type the first number : ";
+        std::cin >> num1;
+        std::cout << "\nType the second number : ";
+        std::cin >> num2;
+        if(!num2){return -1;};
+        std::cout << "\n\nA soma -> " << numero1 + numero2;
+        std::cout << "\nA subtracao -> " << numero1 - numero2;
+        std::cout << "\nA divisao -> " << numero1 / numero2;
+        std::cout << "\nA multiplicacao -> "<< numero1 * numero2;
+    }while(true);
+    return 0;
 }
